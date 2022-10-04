4fdf38ebc17dbd3c485f2b50d3eb88b1578b393e
Radek Zahradník <kutlime@gmail.com>
2022-10-04 19:34:52 +0200

    Simply the loop body

--- Finished sample/FactoryMethod/Program.cs
+++ Finished sample/FactoryMethod/Program.cs
@@ -4,9 +4,8 @@ var factories = new List<DiscountFactory> {
     new CodeDiscountFactory(Guid.NewGuid()), 
     new CountryDiscountFactory("BE") };
 
-foreach (var factory in factories)
+foreach (var discountService in factories.Select(factory => factory.CreateDiscountService()))
 {
-    var discountService = factory.CreateDiscountService();
     Console.WriteLine($"Percentage {discountService.DiscountPercentage} coming from {discountService}");
 } 
 
